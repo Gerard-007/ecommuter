780ae146a0267b901f90f9a41f486fd95077474f
Gerard <Danchuksm@gmail.com>
2019-09-02 01:49:48 +0100

    Uploading the project

---
+++ src/accounts/models.py
@@ -0,0 +1,91 @@
+from django.contrib.auth.models import (
+	AbstractBaseUser,
+	BaseUserManager,
+	PermissionsMixin,
+)
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+def upload_dir(self, filename):
+    return "{}/{}".format(self.username, filename)
+
+
+class UserManager(BaseUserManager):
+
+	def create_user(self, email, username, mobile, display_name=None, password=None):
+		'''
+		Raising ValueError for the user model
+		'''
+		if not email:
+			raise ValueError("Users must have an email address")
+		if not mobile:
+			raise ValueError("Users must have an mobile number")
+		if not display_name:
+			display_name = username
+
+		'''
+		Making a new user instance which is stored in memory only...
+		'''
+		user = self.model(
+			email = self.normalize_email(email),
+			username = username,
+			mobile = mobile,
+			display_name = display_name,
+		)
+		user.set_password(password)
+		user.save()
+		return user
+
+
+	def create_superuser(self, email, username, mobile, display_name, password):
+		'''
+		Here we pass the create_user() method above to create super_user
+		'''
+		user = self.create_user(
+			email,
+			username,
+			mobile,
+			display_name,
+			password,
+		)
+		user.is_staff = True
+		user.is_superuser = True
+		user.save()
+		return user
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+	email = models.EmailField(unique=True)
+	username = models.CharField(max_length=40, unique=True)
+	mobile = models.IntegerField(unique=True)
+	display_name = models.CharField(max_length=150)
+	bio = models.CharField(max_length=150, blank=True, default="")
+	avatar = models.ImageField(blank=True, null=True, upload_to=upload_dir)
+	date_joined = models.DateTimeField(default=timezone.now)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = "username"
+	REQUIRED_FIELDS = ["email", "mobile", "display_name"]
+
+	def __str__(self):
+		return '@{}'.format(self.username)
+	
+	@property
+	def image_url(self):
+		if self.avatar:
+			return self.avatar.url
+		else:
+			#return 'https://res.cloudinary.com/hwz12fud7/image/upload/v1538131159/media/musicadence/musicadence.jpg'
+			return "static/img/faces/user.png"
+
+	def get_short_name(self):
+		return self.display_name
+
+	def get_long_name(self):
+		return '{} (@{})'.format(self.display_name, self.username)
---
+++ src/accounts/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import login, logout
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.generic import FormView, RedirectView, CreateView
+
+from . import forms
+
+
+
+class LoginView(FormView):
+	form_class = AuthenticationForm
+	success_url = reverse_lazy("home")
+	template_name = "accounts/login.html"
+
+	def get_form(self, form_class=None):
+		if form_class is None:
+			form_class = self.get_form_class()
+		return form_class(self.request, **self.get_form_kwargs())
+
+	def form_valid(self, form):
+		login(self.request, form.get_user())
+		return super().form_valid(form)
+
+
+class LogoutView(RedirectView):
+	url = reverse_lazy("home")
+
+	def get(self, request, *args, **kwargs):
+		logout(request)
+		return super().get(self, request, *args, **kwargs)
+
+
+class SignUpView(CreateView):
+	form_class = forms.UserCreateForm
+	success_url = reverse_lazy("login")
+	template_name = "accounts/signup.html"
